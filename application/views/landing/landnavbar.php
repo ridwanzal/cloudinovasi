1da3974c39389591bc2d7d83c6db4a80afa2175a
M. Ridwan Zalbina <[zalbinaridwan@gmail.com]>
2019-07-24 19:26:55 +0700

    navbar

--- application/views/landing/landnavbar.php
+++ application/views/landing/landnavbar.php
@@ -1,41 +1,40 @@
-<nav class="navbar navbar-expand-sm sticky-top  bg-light navbar-light">
-  <!-- Brand/logo -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
   <a class="navbar-brand" href="<?php echo base_url() ?>" title="Cloud Inovasi">
     <img src="<?php echo base_url() ?>assets/images/logo.png" height="22">
   </a>
-  
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>" data-toggle="tooltip" title="Home" >Home</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>plans/" data-toggle="tooltip" title="Plans">Plans</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>faq/" data-toggle="tooltip" title="FAQ">FAQ</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>blog/" data-toggle="tooltip" title="Blog">Blog</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>partner/" data-toggle="tooltip" title="Partner">Partner</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>docs/" data-toggle="tooltip" title="Docs">Docs</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>pricing/" data-toggle="tooltip" title="Pricing">Pricing</a>
-    </li>
-    <li class="nav-item px-2">
-      <a class="nav-link" href="<?php echo base_url() ?>pricing/" data-toggle="tooltip" title="Contact">Contact Us</a>
-    </li>
-  </ul>
 
-  <!-- links -->
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-    <a href="<?php echo base_url() ?>login/"><button type="button" class="btn btn-primary btn-dark">Sign In</button></a>
-    </li>
-  </ul>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>" data-toggle="tooltip" title="Home" >Home</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>plans/" data-toggle="tooltip" title="Plans">Plans</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>faq/" data-toggle="tooltip" title="FAQ">FAQ</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>blog/" data-toggle="tooltip" title="Blog">Blog</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>partner/" data-toggle="tooltip" title="Partner">Partner</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>docs/" data-toggle="tooltip" title="Docs">Docs</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>pricing/" data-toggle="tooltip" title="Pricing">Pricing</a>
+      </li>
+      <li class="nav-item px-2">
+        <a class="nav-link" href="<?php echo base_url() ?>pricing/" data-toggle="tooltip" title="Contact">Contact Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <a href="<?php echo base_url() ?>login/"><button type="button" class="btn btn-primary btn-dark">Sign In</button></a>
+    </form>
+  </div>
 </nav>
